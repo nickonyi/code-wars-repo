0d5e9475160e1161a5a37109cdf575bd85373638
phatommobb <nickoluoch1130@gmail.com>
2025-01-30 22:17:20 +0300

    some code

--- jan/code1.js
+++ jan/code1.js
@@ -85,5 +85,8 @@ const sumItem = (array) => {
 function sortByLength(array) {
   // Return an array containing the same strings,
   // ordered from shortest to longest
-  return [];
+  array.sort((a, b) => a.length - b.length);
+  return array;
 }
+
+console.log(sortByLength(['Beg', 'Life', 'I', 'To']));

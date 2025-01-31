56d84925799255827e69cedc2920fe76ee0a7d03
Nick onyi <nickoluoch1130@gmail.com>
2025-01-31 20:24:39 +0300

    some hard level shit

--- jan/code.js
+++ jan/code.js
@@ -3,9 +3,22 @@ function mix(s1, s2) {
     //convert the strings to lowercase letter and only have letters in every string
     let string1 =s1.toLowerCase().replace(/[^a-zA-Z]/g, '');
     let string2 =s2.toLowerCase().replace(/[^a-zA-Z]/g, '');
+    //count the frequency of every letter
 
-    console.log([string1,string2]);
+    const frequency1 = [...string1].reduce((frequency,char)=> {
+        frequency[char]=(frequency[char] || 0)+1
+        return frequency;
+    },{});
+
+    const frequency2 = [...string2].reduce((frequency,char)=> {
+        frequency[char]=(frequency[char] || 0)+1
+        return frequency;
+    },{});
+
+    //remove items that don't appear more that two times
+    return [frequency1,frequency2];
     
   }
 
-  mix("my&friend&Paul has heavy hats! &","my friend John has many many friends &")
+ console.log
+ ( mix("my&friend&Paul has heavy hats! &","my friend John has many many friends &"))

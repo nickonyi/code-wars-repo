549bd52b09e410beb6dbcb458b1b7dee728b6634
phatommobb <nickoluoch1130@gmail.com>
2025-02-06 20:08:53 +0300

    some code

--- feb/wk1.js
+++ feb/wk1.js
@@ -29,4 +29,11 @@ console.log(
 
 function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
   // your code
+  let predictedAge = [age1, age2, age3, age4, age5, age6, age7, age8];
+  let predictedAgeDoubled = predictedAge.map((num) => num * num);
+
+  const sumAge = predictedAgeDoubled.reduce((acc, num) => acc + num, 0);
+  return Math.floor(Math.sqrt(sumAge) / 2);
 }
+
+console.log(predictAge(65, 60, 75, 55, 60, 63, 64, 45));

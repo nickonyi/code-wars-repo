079a505f52148599049884a898f7a12f09fa8fbe
phatommobb <nickoluoch1130@gmail.com>
2025-02-06 19:56:24 +0300

    some code

--- feb/wk1.js
+++ feb/wk1.js
@@ -26,3 +26,7 @@ console.log(
     'aooaooihbuwuoibieauanqriorixtyubieujlasiamjgeauraroieeduaiiaaijjoubaxaoqgnoieauzeaioeumieuijcoeleoeiguiihaaaaabudwckqoifsaexbeggxlwdahioyuyueairaaraqzbfwqglkhfiyauu'
   )
 );
+
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  // your code
+}

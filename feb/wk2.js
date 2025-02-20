603a1162dec8a5e26ccd5187a20c151945b3e051
phatommobb <nickoluoch1130@gmail.com>
2025-02-20 19:04:30 +0300

    codewars

--- feb/wk2.js
+++ feb/wk2.js
@@ -76,4 +76,17 @@ function cockroachSpeed(s) {
   return Math.floor((s * 100000) / 3600);
 }
 
-console.log(cockroachSpeed(1.08));
+function countRedBeads(n) {
+  //your code here
+  if (n < 2) {
+    return 0;
+  }
+  let redBeads = [];
+
+  for (let i = 2; i <= n; i++) {
+    redBeads.push('r', 'r');
+  }
+  return redBeads.length;
+}
+
+console.log(countRedBeads(5));

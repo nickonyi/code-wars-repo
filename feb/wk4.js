e73e5fc080f4005ef8177cfbdf639ac0d2a48ace
phatommobb <nickoluoch1130@gmail.com>
2025-02-26 16:09:34 +0300

    codewars code

--- feb/wk4.js
+++ feb/wk4.js
@@ -1,4 +1,4 @@
-function highestRank(arr) {
+function highestRankOld(arr) {
   //Your Code logic should written here
   //Use an object to count the frequency of the numbers
   let frequency = {};
@@ -22,6 +22,23 @@ function highestRank(arr) {
   }
 
   return mostFrequentNumber;
+}
+
+//highset rank refactored
+function highestRank(arr) {
+  const freq = arr.reduce((a, b) => ((a[b] = ++a[b] || 1), a), {});
+
+  let max = 0;
+  let num = 0;
+
+  for (const k in freq) {
+    if (freq[k] >= max) {
+      max = freq[k];
+      num = Math.max(+k, num);
+    }
+  }
+
+  return num;
 }
 
 console.log(highestRank([12, 10, 10, 8, 12, 7, 6, 4, 10, 12]));

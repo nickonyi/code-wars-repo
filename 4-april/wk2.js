4b7801f86c49f2c14736b50881b542e9a2256e3b
phatommobb <nickoluoch1130@gmail.com>
2025-04-22 14:30:02 +0300

    new code

--- 4-april/wk2.js
+++ 4-april/wk2.js
@@ -1,7 +1,25 @@
 //we want to know the index of vowels
 function vowelIndices(word) {
   //your code here
-  return word[0];
+  //initialize an array
+  const result = [];
+  //convert to lowercase
+  word = word.toLowerCase();
+  //loop over the array
+  for (let i = 0; i < word.length; i++) {
+    //if I find a vowel I send it to my result
+    if (
+      word[i] === 'a' ||
+      word[i] === 'e' ||
+      word[i] === 'i' ||
+      word[i] === 'o' ||
+      word[i] === 'u' ||
+      word[i] === 'y'
+    ) {
+      result.push(i + 1);
+    }
+  }
+  return result;
 }
 
-console.log(vowelIndices('super'));
+console.log(vowelIndices('supEr'));

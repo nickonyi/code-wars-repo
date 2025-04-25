35bec10c352d5c31b993864b293fba26a8182e49
phatommobb <nickoluoch1130@gmail.com>
2025-04-25 16:15:12 +0300

    code

--- 4-april/wk3.js
+++ 4-april/wk3.js
@@ -1,3 +1,15 @@
 function giveMeFive(obj) {
   //coding here
+  const result = [];
+  for (const key in obj) {
+    if (key.length == 5) {
+      result.push(key);
+    }
+    if (obj[key].length === 5) {
+      result.push(obj[key]);
+    }
+  }
+  return result;
 }
+
+console.log(giveMeFive({ Our: 'earth', is: 'a', beautyful: 'world' }));

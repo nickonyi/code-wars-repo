3fc8f39d27ec7c70aa5e8cbc3c323bf7680bea1c
phatommobb <nickoluoch1130@gmail.com>
2025-05-02 14:47:12 +0300

    new reducer code

--- 5-may/wk1.js
+++ 5-may/wk1.js
@@ -11,4 +11,31 @@ function evenNumbers(array, number) {
   return evenNarr.slice(-number);
 }
 
-console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+//console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+
+function solve(s) {
+  //given a string I compre the number of lower case letters and uppercase letters
+  //if lowercase letters is > uppper case letters turn the the upper case letters to lowe case
+  //if the uppercase > lowercase turn the remaining lowercase letter to uppercase
+  //if uppercase = lowercase letter the whole letters should be lowercase
+
+  //count the small letters and capial letters
+  let smallL = 0;
+  let capitalL = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i].charCodeAt(0) >= 65 && s[i].charCodeAt(0) <= 90) {
+      capitalL++;
+    } else if (s[i].charCodeAt(0) >= 97 && s[i].charCodeAt(0) <= 122) {
+      smallL++;
+    }
+  }
+  if (smallL > capitalL) {
+    return s.toLowerCase();
+  } else if (capitalL > smallL) {
+    return s.toUpperCase();
+  } else if (smallL === capitalL) {
+    return s.toLowerCase();
+  }
+}
+
+console.log(solve('CODe'));

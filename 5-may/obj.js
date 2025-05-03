9f236cd4629d3e506a860ca63ae63a29dacc3d5d
phatommobb <nickoluoch1130@gmail.com>
2025-05-03 16:07:41 +0300

    code wars

---
+++ 5-may/obj.js
@@ -0,0 +1,31 @@
+Array.prototype.sum = function () {
+  return this.reduce((acc, num) => acc + num, 0);
+};
+
+Array.prototype.cube = function () {
+  return this.map((i) => i ** 3);
+};
+Array.prototype.square = function () {
+  return this.map((i) => i ** 2);
+};
+
+Array.prototype.average = function () {
+  if (this.length === 0) {
+    return NaN;
+  }
+  return this.reduce((acc, num) => acc + num, 0) / this.length;
+};
+Array.prototype.even = function () {
+  return this.filter((i) => i % 2 == 0);
+};
+Array.prototype.odd = function () {
+  return this.filter((i) => i % 2 !== 0);
+};
+
+let numbers = [1, 2, 3, 4, 5];
+const wod = [];
+console.log(numbers.sum());
+console.log(numbers.cube());
+console.log(wod.average());
+console.log(numbers.even());
+console.log(numbers.odd());

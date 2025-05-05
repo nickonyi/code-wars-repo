8673af6b4f6167b251cb19f6ced376a45079c1ea
phatommobb <nickoluoch1130@gmail.com>
2025-05-05 18:34:29 +0300

    week 2 code

--- 5-may/wk2.js
+++ 5-may/wk2.js
@@ -22,4 +22,11 @@ function dashatizee(num) {
   return dashedArr.join('').replace(/--+/g, '-');
 }
 
-console.log(dashatize(-1));
+function dashatize(num) {
+  return String(num)
+    .replace(/([13579])/g, '-$1-')
+    .replace(/--+/g, '-')
+    .replace(/(^-|-$)/g, '');
+}
+
+console.log(dashatize(68251));

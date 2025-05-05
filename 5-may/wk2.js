33a4b966c13026d5f982888924c8381a90a7074e
phatommobb <nickoluoch1130@gmail.com>
2025-05-05 18:30:37 +0300

    week 2 code

---
+++ 5-may/wk2.js
@@ -0,0 +1,25 @@
+function dashatizee(num) {
+  //turn the number into a sting array
+  num = Math.abs(num);
+  const numArr = String(num).split('');
+  //traverse through the loop
+  const dashedArr = numArr.map((n, i, arr) => {
+    //if a number is odd add dash at the start and the end
+    if (n % 2 !== 0) {
+      if (arr.length === 1) {
+        return n;
+      } else if (i == 0) {
+        return n + '-';
+      } else if (i === arr.length - 1) {
+        return '-' + n;
+      }
+      return '-' + n + '-';
+    } else {
+      return n;
+    }
+  });
+  //join the array into a string and return
+  return dashedArr.join('').replace(/--+/g, '-');
+}
+
+console.log(dashatize(-1));

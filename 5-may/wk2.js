3fdc07a5521840fd9b9ddb5da9f7a78d0da410e9
phatommobb <nickoluoch1130@gmail.com>
2025-05-07 16:01:33 +0300

    code

--- 5-may/wk2.js
+++ 5-may/wk2.js
@@ -32,8 +32,44 @@ function dashatize(num) {
 function balancedNum(number) {
   //convert the number into a string array
   const numbStr = String(number);
-  //find the middle number
+  //extract the left numbers
+  const left = splitAroundMiddle(numbStr).left.split('').map(Number);
+
+  //extract the right numbers
+  const right = splitAroundMiddle(numbStr).right.split('').map(Number);
+
   //add numbers to the left of the array
+  const leftSum = left.reduce((acc, num) => acc + num, 0);
+
   //add numbers to the right of the middle element
+  const rightSum = right.reduce((acc, num) => acc + num, 0);
+
   //compare the two if they are equal return balance otherwise they are unbalanced
+  if (leftSum === rightSum) {
+    return 'Balanced';
+  }
+  return 'Not balanced';
 }
+
+const splitAroundMiddle = (arr) => {
+  const len = arr.length;
+  const isEven = len % 2 == 0;
+  if (isEven) {
+    const mid1 = len / 2 - 1;
+    const mid2 = len / 2;
+    return {
+      left: arr.slice(0, mid1),
+      middle: arr.slice(mid1, mid2),
+      right: arr.slice(mid2 + 1),
+    };
+  } else {
+    const mid = Math.floor(len / 2);
+    return {
+      left: arr.slice(0, mid),
+      middle: [arr[mid]],
+      right: arr.slice(mid + 1),
+    };
+  }
+};
+
+console.log(balancedNum(56239814));

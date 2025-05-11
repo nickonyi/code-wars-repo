91cef69f93f9598e5bcf88704e8881d1bdf651f0
phatommobb <nickoluoch1130@gmail.com>
2025-05-11 22:41:12 +0300

    code

--- 5-may/wk2.js
+++ 5-may/wk2.js
@@ -140,3 +140,7 @@ function adjacentElementsProduct(array) {
 }
 
 console.log(adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921]));
+
+function automorphic(n) {
+  return 'Not!!'; // your code here
+}

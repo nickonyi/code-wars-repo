e20c40a183d08b7dc34e2d91fe7461bc53c314a0
phatommobb <nickoluoch1130@gmail.com>
2025-05-13 14:51:18 +0300

    code

--- 5-may/wk3.js
+++ 5-may/wk3.js
@@ -1,3 +1,18 @@
 function upArray(arr) {
   // ...
+  //combine the values of the array into a single digit
+
+  let number = Number(arr.join(''));
+  console.log(number);
+
+  //add one to the single digit
+  number = number + 1;
+  //return the array of individual values
+
+  return arr[0] == 0 ? [0, number] : String(number).split('').map(Number);
 }
+
+//console.log(upArray([4, 3, 2, 5]));
+console.log(
+  upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+);

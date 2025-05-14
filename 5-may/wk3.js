38adf9b7e0cd5e510ce450dff9385d1c3e329b81
phatommobb <nickoluoch1130@gmail.com>
2025-05-14 21:03:11 +0300

    code

--- 5-may/wk3.js
+++ 5-may/wk3.js
@@ -19,8 +19,4 @@ function upArray(arr) {
   return resultStr.split('').map(Number);
 }
 
-//console.log(upArray([4, 3, 2, 5]));
-console.log(
-  upArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-);
-console.log([0, 7]);
+function outed(meet, boss) {}

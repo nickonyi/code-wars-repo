7fa82204010e42873ded6de7d4bf2977d268c6bb
phatommobb <nickoluoch1130@gmail.com>
2025-05-14 21:33:43 +0300

    code

--- 5-may/wk3.js
+++ 5-may/wk3.js
@@ -19,4 +19,42 @@ function upArray(arr) {
   return resultStr.split('').map(Number);
 }
 
-function outed(meet, boss) {}
+function outed(meet, boss) {
+  //loop over the object and collect the hapinness ratings
+  let hapinness = [];
+  for (const key in meet) {
+    if (key === boss) {
+      meet[key] *= 2;
+    }
+    hapinness.push(meet[key]);
+  }
+  //get the average hapiness ratngg
+  const hapinnessRating = Math.floor(
+    hapinness.reduce((a, b) => a + b, 0) / hapinness.length
+  );
+  //determine if you leave the room or not
+  if (hapinnessRating <= 5) {
+    return 'Get Out Now!';
+  } else {
+    return 'Nice Work Champ!';
+  }
+}
+
+console.log(
+  outed(
+    {
+      tim: 0,
+      jim: 2,
+      randy: 0,
+      sandy: 7,
+      andy: 0,
+      katie: 5,
+      laura: 1,
+      saajid: 2,
+      alex: 3,
+      john: 2,
+      mr: 0,
+    },
+    'laura'
+  )
+);

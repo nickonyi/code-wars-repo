3c9cccafdfede378cf20a95f44076a48c69cd3d8
phatommobb <nickoluoch1130@gmail.com>
2025-06-01 17:04:26 +0300

    wk 1 code

---
+++ 6-june/wk1.js
@@ -0,0 +1,23 @@
+function abbreviate(string) {
+  // ...
+  if (!string) {
+    return '';
+  }
+  //separate the string in to indidual words if the strng has more than one word
+  const words = string.match(/[A-Za-z]+|[^A-Za-z]+/g);
+  const letters = /^[A-Za-z]+$/;
+
+  //for each of the words if its length is > 4 gets its abbreviation otherwise maintain as it should
+  const abb = words
+    .map((w) => {
+      if (letters.test(w) && w.length >= 4) {
+        return w[0] + (w.length - 2) + w.slice(-1);
+      } else {
+        return w;
+      }
+    })
+    .join('');
+  return abb;
+}
+
+console.log(abbreviate('elephant-rides are really fun!'));

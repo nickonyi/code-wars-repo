eaf5cfd74982f8ef6e635284c3a90e537e152c75
phatommobb <nickoluoch1130@gmail.com>
2025-06-02 23:32:10 +0300

    refactor: improve lovefunc logic and add console log for testing

--- 6-june/wk1.js
+++ 6-june/wk1.js
@@ -23,4 +23,13 @@ function abbreviate(string) {
 console.log(abbreviate('elephant-rides are really fun!'));
 function lovefunc(flower1, flower2) {
   // moment of truth
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower2 % 2 === 0 && flower1 % 2 !== 0)
+  ) {
+    return true;
+  }
+  return false;
 }
+
+console.log(lovefunc(2, 2));

b93e42e29644a98e32c49fb2d230e53ece6ea20e
phatommobb <nickoluoch1130@gmail.com>
2025-06-09 17:04:50 +0300

    feat: add stockList function skeleton

--- 6-june/wk2.js
+++ 6-june/wk2.js
@@ -1,3 +1,30 @@
 function stockList(books, categories) {
-  return '';
+  let total = categories.reduce((acc, cat) => {
+    acc[cat] = 0;
+    return acc;
+  }, {});
+
+  let result = '';
+
+  books.forEach((book) => {
+    const [code, qty] = book.split(' ');
+    const letter = code[0];
+    const amount = Number(qty);
+
+    total[letter] = (total[letter] || 0) + amount;
+  });
+
+  for (const letter in total) {
+    result += `(${letter}:${total[letter]}) -`;
+  }
+
+  result = result.slice(0, -1);
+  return result;
 }
+
+console.log(
+  stockList(
+    ['BBAR 150', 'CDXE 515', 'BKWR 250', 'BTSQ 890', 'DRTY 600'],
+    ['A', 'B', 'C', 'D']
+  )
+);

6c8b6e459760654941ada72d645da8c718fa5c59
phatommobb <nickoluoch1130@gmail.com>
2025-06-27 21:44:27 +0300

    feat: implement chooseBestSum and getCombinations functions for optimal sum selection

--- 6-june/wk4.js
+++ 6-june/wk4.js
@@ -1,3 +1,17 @@
 function race(v1, v2, g) {
-  // your code
+  if (v1 >= v2) return null;
+
+  const time = g / (v2 - v1);
+
+  let hours = Math.floor(time);
+
+  let minutes = Math.floor((time * 60) % 60);
+
+  let seconds = Math.floor((time * 3600) % 60);
+
+  return [hours, minutes, seconds];
 }
+
+console.log(race(720, 850, 70));
+console.log(race(720, 850, 37));
+console.log(race(80, 91, 37));

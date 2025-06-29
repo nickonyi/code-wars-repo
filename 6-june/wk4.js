ea1775003ac9baeea556d156a74559baa3dce660
phatommobb <nickoluoch1130@gmail.com>
2025-06-29 19:00:20 +0300

    feat: implement solve function to count character categories in a string

--- 6-june/wk4.js
+++ 6-june/wk4.js
@@ -13,4 +13,28 @@ function race(v1, v2, g) {
 }
 function solve(s) {
   //..
+  //define variables for the different categories
+  let cap = 0;
+  let small = 0;
+  let specialChars = 0;
+  let nums = 0;
+  //loop over my string
+  for (let i = 0; i < s.length; i++) {
+    //count the number of capital letters,small letters and special characters and add them to final out
+    let code = s[i].charCodeAt(0);
+    if (code >= 65 && code <= 90) {
+      cap++;
+    } else if (code >= 97 && code <= 122) {
+      small++;
+    } else if (code >= 48 && code <= 57) {
+      nums++;
+    } else {
+      specialChars++;
+    }
+  }
+  //return the final output
+  return [cap, small, nums, specialChars];
 }
+
+console.log(solve('aAbBcC'));
+console.log(solve(''));

3eb44d0624aa818cbc36e89c56579a03ee84b154
phatommobb <nickoluoch1130@gmail.com>
2025-07-02 18:49:46 +0300

    feat: add maxTriSum and getCombinations functions with initial implementation

--- 7-july/wk1.js
+++ 7-july/wk1.js
@@ -3,3 +3,26 @@ function multiplyAll(arr) {
     return arr.map((i) => x * i);
   };
 }
+
+function maxTriSum(numbers) {
+  //your code here
+}
+
+function getCombinations(arr, size) {
+  const result = [];
+
+  function backtrack(start, current) {
+    if (current.length === size) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = start; i < arr.length; i++) {
+      current.push(arr[i]);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+
+  backtrack(0, []);
+  return result;
+}

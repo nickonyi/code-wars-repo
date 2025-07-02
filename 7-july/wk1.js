99a2da25657fd6b4873aa0cd2bbd0a49aab10820
phatommobb <nickoluoch1130@gmail.com>
2025-07-02 19:46:43 +0300

    refactor: simplify maxTriSum function by removing unused getCombinations logic and optimizing unique number handling

--- 7-july/wk1.js
+++ 7-july/wk1.js
@@ -4,40 +4,54 @@ function multiplyAll(arr) {
   };
 }
 
-function maxTriSum(numbers, size = 3) {
-  //your code here
+//function maxTriSum(numbers, size = 3) {
+//  //your code here
+//  const unique = [...new Set(numbers)];
+//  console.log(unique);
+//
+//  const arrN = getCombinations(unique, size);
+//  console.log(arrN);
+//
+//  let max = 0;
+//  arrN.forEach((arr) => {
+//    let sum = arr.reduce((a, b) => a + b, 0);
+//    if (sum > max) {
+//      max = sum;
+//    }
+//  });
+//  return max;
+//}
+//
+//function getCombinations(arr, size) {
+//  const result = [];
+//
+//  function backtrack(start, current) {
+//    if (current.length === size) {
+//      result.push([...current]);
+//      return;
+//    }
+//    for (let i = start; i < arr.length; i++) {
+//      current.push(arr[i]);
+//      backtrack(i + 1, current);
+//      current.pop();
+//    }
+//  }
+//
+//  backtrack(0, []);
+//  return result;
+//}
+
+function maxTriSum(numbers) {
   const unique = [...new Set(numbers)];
   console.log(unique);
 
-  const arrN = getCombinations(unique, size);
-  console.log(arrN);
+  const sort = unique.sort((a, b) => a - b);
+  console.log(sort);
 
-  let max = 0;
-  arrN.forEach((arr) => {
-    let sum = arr.reduce((a, b) => a + b, 0);
-    if (sum > max) {
-      max = sum;
-    }
-  });
-  return max;
-}
+  const slice = sort.slice(-3);
+  console.log(slice);
 
-function getCombinations(arr, size) {
-  const result = [];
-
-  function backtrack(start, current) {
-    if (current.length === size) {
-      result.push([...current]);
-      return;
-    }
-    for (let i = start; i < arr.length; i++) {
-      current.push(arr[i]);
-      backtrack(i + 1, current);
-      current.pop();
-    }
-  }
-
-  backtrack(0, []);
-  return result;
+  const sum = slice.reduce((a, b) => a + b);
+  return sum;
 }
 console.log(maxTriSum([-13, -50, 57, 13, 67, -13, 57, 108, 67]));

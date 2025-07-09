eb49525fec3c7bc3075bf8fd0cdf9320ca4ffa85
phatommobb <nickoluoch1130@gmail.com>
2025-07-09 14:47:09 +0300

    feat: implement capitalize function to transform specified indices to uppercase

--- 7-july/wk2.js
+++ 7-july/wk2.js
@@ -1,3 +1,17 @@
 function capitalize(string, indices) {
-  return '';
+  //define your final result variale
+  let result = '';
+
+  //loop through the string array
+  for (let i = 0; i < string.length; i++) {
+    //retrieve the character at the selected index
+    //capitalize the string
+    if (indices.includes(i)) {
+      result += string[i].toUpperCase();
+    } else result += string[i];
+  }
+  //return the new string
+  return result;
 }
+
+console.log(capitalize('abcdef', [1, 2, 5]));

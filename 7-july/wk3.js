1f3c9575b9055af555f5cb1d8858bc0d8b6560d8
phatommobb <nickoluoch1130@gmail.com>
2025-07-14 17:39:05 +0300

    code wk3

---
+++ 7-july/wk3.js
@@ -0,0 +1,24 @@
+function sumDigPow(a, b) {
+  // Your code here
+}
+
+function solve(arr){  
+  //code
+  //define a count variable
+  let count = 0;
+  //define the result array
+  let result = [];
+  //loop over the array and determine the positon of the strings relative to their alphabet position
+  arr.forEach((word)=>{
+    let wordStr = word.split('');
+    wordStr.forEach((char,index)=>{
+        if(index+1 === char.charCodeAt(0)){
+            count++;
+            result.push(code);
+        }
+    })
+  })
+  //count the letters that are in their relative position
+  //return the result array
+return  result;
+};

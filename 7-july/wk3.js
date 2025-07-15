97891f4f2cbd5ec616bd20a36d30eecc4142c16d
phatommobb <nickoluoch1130@gmail.com>
2025-07-15 16:55:18 +0300

    fix: correct string manipulation in vaporcode function

--- 7-july/wk3.js
+++ 7-july/wk3.js
@@ -4,11 +4,15 @@ function sumDigPow(a, b) {
 
 function vaporcode(string) {
  //jooin if there are spaces
- let newString = string.join(' ');
+ let newString = string.split('');
   //turn to upppercase
-  newString = newString.toUpperCase();
+  newString = newString.map(char => char.toUpperCase());
  //add the spaces
+ newString = newString.filter(char => char !== ' ');
+ //join the characters with two spaces
  const result = newString.join('  ');
  //return the string 
  return result;
 }
+
+console.log(vaporcode("Lets go to the movies")); // "L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S"

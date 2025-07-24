b7c86f3fe34cdbc18c8938f8a3f5529e45a40164
phatommobb <nickoluoch1130@gmail.com>
2025-07-24 16:23:00 +0300

    feat: enhance Player class with sayName method and prototype methods

--- patterns/object-construtctors/index.js
+++ patterns/object-construtctors/index.js
@@ -1,4 +1,21 @@
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
+  this.sayName = () => {
+    console.log("say my name " + name);
+  };
 }
+
+Player.prototype.sayHello = () => {
+  console.log("Hello I am a player");
+};
+Player.prototype.hello = "Hello, guys!";
+const player1 = new Player("darius", "o");
+const player2 = new Player("ondusu", "x");
+
+player1.sayName();
+player2.sayName();
+player1.sayHello();
+
+console.log(Object.getPrototypeOf(player1));
+console.log(player1.prototype);

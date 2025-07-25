d1f13d11504f37fccfd38ded36a3db13d741c64d
phatommobb <nickoluoch1130@gmail.com>
2025-07-25 15:43:27 +0300

    refactor: restructure Person and Player constructors and methods for clarity

--- patterns/object-construtctors/index.js
+++ patterns/object-construtctors/index.js
@@ -1,21 +1,16 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayName = () => {
+  console.log(`Hello, I'm ${this.name}!`);
+};
+
 function Player(name, marker) {
   this.name = name;
   this.marker = marker;
-  this.sayName = () => {
-    console.log("say my name " + name);
-  };
 }
 
-Player.prototype.sayHello = () => {
-  console.log("Hello I am a player");
+Player.prototype.getMarker = () => {
+  console.log(`My marker is '${this.marker}'`);
 };
-Player.prototype.hello = "Hello, guys!";
-const player1 = new Player("darius", "o");
-const player2 = new Player("ondusu", "x");
-
-player1.sayName();
-player2.sayName();
-player1.sayHello();
-
-console.log(Object.getPrototypeOf(player1));
-console.log(player1.prototype);

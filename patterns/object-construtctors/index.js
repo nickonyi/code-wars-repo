1b70ee8bbaffbe5977723e50fe05030957c172ad
phatommobb <nickoluoch1130@gmail.com>
2025-07-25 16:07:47 +0300

    refactor: replace arrow functions with regular functions for prototype methods

--- patterns/object-construtctors/index.js
+++ patterns/object-construtctors/index.js
@@ -2,7 +2,7 @@ function Person(name) {
   this.name = name;
 }
 
-Person.prototype.sayName = () => {
+Person.prototype.sayName = function () {
   console.log(`Hello, I'm ${this.name}!`);
 };
 
@@ -11,6 +11,21 @@ function Player(name, marker) {
   this.marker = marker;
 }
 
-Player.prototype.getMarker = () => {
+Player.prototype.getMarker = function () {
   console.log(`My marker is '${this.marker}'`);
 };
+
+//console.log(Object.getPrototypeOf(Player.prototype));
+//console.log(Object.prototype);
+
+//Object.setPrototypeOf(Player.prototype, Person.prototype);
+Player.prototype = Person.prototype;
+//console.log(Object.getPrototypeOf(Player.prototype));
+
+const player1 = new Player("Malcom", "x");
+const player2 = new Player("Joel", "O");
+
+player1.sayName();
+player1.getMarker();
+player2.sayName();
+player2.getMarker();

6d72ba16b95b5a7643a1873ab8aeaac297623b20
nickonyi <nickoluoch1130@gmail.com>
2025-09-10 15:46:04 +0300

    code for week 2 september

---
+++ 9-sept/wk2.js
@@ -0,0 +1,14 @@
+//Given an array of stringed integers add up the non stringed intergers
+//and subtract the total of nun stringed intergers
+function divCon(x){
+//divide the array between stringed and non stringed integer
+const NonS = x.filter(n=> typeof n === "number");
+const strN =  x.filter(n=> typeof n !== "number").map(Number);
+//add them up 
+const sumNos = NonS.reduce((a,b)=> a+b,0);
+const sumStrN = strN.reduce((a,b)=>a+b,0);
+//and then subtract
+return sumNos -sumStrN;
+}
+
+console.log(divCon([9, 3, '7', '3']));

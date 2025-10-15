313320740e0188371a130ef2f30e61434fd15410
nickonyi <nickoluoch1130@gmail.com>
2025-10-15 15:16:50 +0300

    Add skeleton for sudoku function to solve 9x9 puzzles

--- 10-oct/wk1.js
+++ 10-oct/wk1.js
@@ -17,3 +17,8 @@ function calc(x) {
 }
 
 console.log(calc("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+
+//wr
+function sudoku(puzzle) {
+  //return the solved puzzle as a 2d array of 9 x 9
+}

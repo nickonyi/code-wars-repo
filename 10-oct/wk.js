16d294590f3f6bce4731c6caa03bde1f407d5fe8
nickonyi <nickoluoch1130@gmail.com>
2025-10-25 10:05:00 +0300

    october code

--- 10-oct/wk.js
+++ 10-oct/wk.js
@@ -9,6 +9,21 @@ function uefaEuro2016(teams, scores) {
   }
 }
 
+//we get an a string and then we truncate into to two letters and return an array containaing the name and the truncated name
 function whoIsPaying(name) {
   //your code here
+  //define an end array
+  let result = [];
+  //if the name is 2 letters or less push only the name in the array
+  if (name.length <= 2) {
+    result.push(name);
+  }
+  //if not push the full name and the truncated letter
+  else {
+    result.push(name, name.slice(0, 2));
+  }
+  //return the result
+  return result;
 }
+
+console.log(whoIsPaying("Mexico"));

c148b00d6aee56cc34d1cd124268cdd5f62d49ad
nickonyi <nickoluoch1130@gmail.com>
2025-10-28 13:27:27 +0300

    derive function

--- 10-oct/lastwiki.js
+++ 10-oct/lastwiki.js
@@ -1,4 +1,11 @@
-function guessBlue(blueStart, redStart, bluePulled, redPulled) {
-  // Your code here.
-    return 0.0;
+//input is the two numbers
+//output is the coefficient multiplied by exponent minus 1 exponent
+
+function derive(coefficient, exponent) {
+  //multiply the coefficent and the exponent
+  const multiplied = coefficient * exponent;
+  //subtract one from the coefficent
+  const newExp = exponent - 1;
+  //return the required expression
+  return `${multiplied}x^${newExp}`;
 }

192fee1546fa4cbedf7123098a18dd1d0ef3ef56
nickonyi <nickoluoch1130@gmail.com>
2025-11-01 13:22:47 +0300

    last code

--- 11-nov/wk1.js
+++ 11-nov/wk1.js
@@ -1,1 +1,21 @@
-function last() {}
+//the input could be a string,an array, two arguments my goal is to return the last item
+function last(...args) {
+  if (args.length > 1) {
+    return args[args.length - 1];
+  }
+
+  const value = args[0];
+  if (typeof value === "string" || Array.isArray(value)) {
+    return value[value.length - 1];
+  }
+
+  if (typeof value === "number") {
+    const numArray = value.toString();
+    if (numArray.length == 1) return parseInt(numArray);
+    return numArray[numArray.length - 1];
+  }
+
+  return null;
+}
+
+console.log(last(5, 5, 7));

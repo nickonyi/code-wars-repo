b90dcc4f56ba22d44c69398b56f9e133a147260f
nickonyi <nickoluoch1130@gmail.com>
2025-11-14 14:12:16 +0300

    is it bugged code

--- 11-nov/wk2.js
+++ 11-nov/wk2.js
@@ -146,3 +146,10 @@ function backToTheFuture(str) {
 }
 
 console.log(backToTheFuture("Tuesday 30 March"));
+
+
+function isItBugged(code){
+//hmm, Looks like its bugged man!
+ return this./^(\d{2}\d-){
+\d{2} \d\d;:;\d\d/".test[date];
+}

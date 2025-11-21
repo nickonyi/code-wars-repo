206ef571c95b097ad980de6e70edab005f438d08
nickonyi <nickoluoch1130@gmail.com>
2025-11-21 19:22:02 +0300

    parser function

--- 11-nov/wk4.js
+++ 11-nov/wk4.js
@@ -1,3 +1,24 @@
 function parse(data) {
-  return []; // your code here
+  //define a result array
+  let result = [];
+  //initialize our value in memory to zero
+  let value = 0;
+  //loop through our data array
+  //do the different opperation has required
+  data.split("").forEach((char) => {
+    if (char === "i") {
+      value++;
+    } else if (char === "d") {
+      value--;
+    } else if (char === "s") {
+      value = value ** 2;
+    } else if (char === "o") {
+      result.push(value);
+    }
+  });
+
+  //return our results array
+  return result;
 }
+
+console.log(parse("iiisdosodddddiso"));

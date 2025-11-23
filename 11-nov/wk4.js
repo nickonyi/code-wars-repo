f9935e54fe240b0b51fa0b39f4dd42e76a20f245
nickonyi <nickoluoch1130@gmail.com>
2025-11-23 15:11:29 +0300

    get michael last name

--- 11-nov/wk4.js
+++ 11-nov/wk4.js
@@ -23,6 +23,29 @@ function parse(data) {
 
 console.log(parse("iiisdosodddddiso"));
 
+//input is a string containing michaels in them
+//I should return an array containing the last name for michael
 function getMichaelLastName(inputText) {
   // your awesome code here
+  //define my results array
+  let results = [];
+  //split my string into an array
+  const inputTextArr = inputText.split(/[^A-Za-z]+/).filter(Boolean);
+
+  //If a string is equal to michael i only pick the next item if it starts with a capital letter
+  //and add them to my results array
+  for (let i = 0; i < inputTextArr.length; i++) {
+    if (inputTextArr[i].toLowerCase() === "michael") {
+      const next = inputTextArr[i + 1];
+
+      if (next && /^[A-Z]/.test(next)) {
+        results.push(next);
+      }
+    }
+  }
+  //return my results array
+  return results;
 }
+const inputText =
+  "Michael, how are you? - Cool, how is John Williamns and Michael Jordan? I don't know but Michael Johnson is fine. Michael do you still score points with LeBron James, Michael Green AKA Star and Michael Wood?";
+console.log(getMichaelLastName(inputText));

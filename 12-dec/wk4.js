a6b8b4e56d5c53529280abc8f767b581cbed1a2e
nickonyi <nickoluoch1130@gmail.com>
2025-12-22 20:27:14 +0300

    code for week 4

--- 12-dec/wk4.js
+++ 12-dec/wk4.js
@@ -1,3 +1,22 @@
 function evil(n) {
-  return "Desired Result";
+  //given a decimal number convert into binary
+  //convert the binary into a string
+  const binary = n.toString(2);
+
+  //define a count variable
+  let binaryCount = 0;
+  //loop through the array and check the number of ones
+  for (let i = 0; i < binary.length; i++) {
+    if (parseInt(binary[i]) === 1) {
+      binaryCount++;
+    }
+  }
+  //determine if it is odious or evil
+
+  if (binaryCount % 2 === 0) {
+    return "It's Evil!";
+  }
+  return "It's Odious!";
 }
+
+console.log(evil(3));
